3388c1feafa0821180cd745ce3bfe97c0bf4f63c
Isaac Ayodele <ziggycodes25@gmail.com>
2023-06-06 06:20:22 +0100

    added all necessary files, dependencies and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "roboto-regular": ["Roboto-Regular"],
+        "roboto-bold": ["Roboto-Bold"]
+      }
+    },
+  },
+  plugins: [],
+}
+
